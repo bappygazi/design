fa09e83eeb9dcbe6a150c2a9ba36bbb685069abf
sakaethossain <sakaethossain122@gmail.com>
2022-01-10 03:51:43 +0600

    skaskljfsa

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+/* JS */
+$(function () {
+  //    sticky navbar
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    var sticky = $(".navbar");
+    if (scrolling >= 20) {
+      sticky.addClass("navbg");
+    } else {
+      sticky.removeClass("navbg");
+    }
+  });
+
+  //    navbar logo change
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    if (scrolling >= 30) {
+      $(".logo").addClass("logo-block");
+    } else {
+      $(".logo").removeClass("logo-block");
+    }
+  });
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    if (scrolling >= 30) {
+      $(".logo-white").addClass("logo-white-none");
+    } else {
+      $(".logo-white").removeClass("logo-white-none");
+    }
+  });
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    if (scrolling >= 30) {
+      $(".navbar-nav").removeClass("golden-color");
+    } else {
+      $(".navbar-nav").addClass("golden-color");
+    }
+  });
+  //navrbar text color black
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    if (scrolling >= 30) {
+      $(".nav-link ").addClass("text-black");
+    } else {
+      $(".nav-link").removeClass("text-black");
+    }
+  });
+});
